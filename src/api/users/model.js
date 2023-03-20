571fd5f2fc7d1072a04a2f2887dcebd05962f1b0
M Yasir Ozdemir <yasiryksmail@gmail.com>
2023-03-20 12:09:09 +0300

    typo fixed

--- src/api/users/model.js
+++ src/api/users/model.js
@@ -1,30 +1,37 @@
-import mongoose, { model } from "mongoose"
+import mongoose, { model } from "mongoose";
 
-const validateEmail = function(email) {
-    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return re.test(email)
-}
-const {Schema}=mongoose
+const validateEmail = function (email) {
+  const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+const { Schema } = mongoose;
 
-const UsersSchema=new Schema({
-    name:{ type: String, required: true , minLength: 3, maxLength: 12},
-    surname:{type:String,required:true,minLength: 3, maxLength: 12},
+const UsersSchema = new Schema(
+  {
+    name: { type: String, required: true, minLength: 3, maxLength: 12 },
+    surname: { type: String, required: true, minLength: 3, maxLength: 12 },
     email: {
-        type: String,
-        trim: true,
-        lowercase: true,
-        unique: true,
-        required: 'Email address is required',
-        validate: [validateEmail, 'Please fill a valid email address'],
-        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: "Email address is required",
+      validate: [validateEmail, "Please fill a valid email address"],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
     },
-    bio:{type:String,required:true,minLength: 10, maxLength: 60},
-    title:{type:String,required:true,minLength: 3, maxLength: 12},
-    area:{type:String,required:true,minLength: 3, maxLength: 12},
-    image:{type:String, default:""},
-    posts:[{  posts:{type:mongoose.Types.ObjectId,required:true, ref:"Post"}}],
-    experiences:[{  experiences:{type:mongoose.Types.ObjectId,required:true, ref:"Expereince"}}]
+    bio: { type: String, required: true, minLength: 10, maxLength: 60 },
+    title: { type: String, required: true, minLength: 3, maxLength: 12 },
+    area: { type: String, required: true, minLength: 3, maxLength: 12 },
+    image: { type: String, default: "" },
+    posts: [{ type: mongoose.Types.ObjectId, required: true, ref: "Post" }],
+    experiences: [
+      { type: mongoose.Types.ObjectId, required: true, ref: "Experience" },
+    ],
+  },
+  { timestamps: true }
+);
 
-},{timestamps:true})
-
-export default model("User", UsersSchema)
+export default model("User", UsersSchema);
