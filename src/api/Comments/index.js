dbc22534a8b1a2708c2eb827258511bc8481bd7d
M Yasir Ozdemir <yasiryksmail@gmail.com>
2023-03-21 18:00:07 +0300

    adds getCommentsWithUserDetails :green_book:

--- src/api/Comments/index.js
+++ src/api/Comments/index.js
@@ -1,98 +1,129 @@
-import  Express  from "express";
-import createHttpError from "http-errors"
-import commentSchema from "./model.js"
-import postModel from "../posts/model.js"
-
+import Express from "express";
+import createHttpError from "http-errors";
+import commentSchema from "./model.js";
+import postModel from "../posts/model.js";
 
 export const isPostExisted = async (req, res, next) => {
-    const user = await postModel.findById(req.params.postId);
-    if (user) next();
-    else
-      next(createHttpError(404, `User with id ${req.params.postId} not found!`));
+  const user = await postModel.findById(req.params.postId);
+  if (user) next();
+  else
+    next(createHttpError(404, `User with id ${req.params.postId} not found!`));
+};
+
+const commentRouter = Express.Router();
+
+commentRouter.post(
+  "/posts/:postId/comments",
+  isPostExisted,
+  async (req, res, next) => {
+    try {
+      const newComment = new commentSchema({
+        post: req.params.postId,
+        ...req.body,
+      });
+      const { _id } = await newComment.save();
+
+      const updatedPost = await postModel.findByIdAndUpdate(
+        req.params.postId,
+        { $push: { comments: _id } },
+        { new: true, runValidators: true }
+      );
+      res.status(201).send(updatedPost);
+    } catch (err) {
+      next(err);
+    }
   }
+);
 
-const commentRouter=Express.Router()
+commentRouter.get(
+  "/posts/:postId/comments",
+  isPostExisted,
+  async (req, res, next) => {
+    try {
+      const Comments = await commentSchema.getCommentsWithUserDetails();
+      res.send(Comments);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
-
-commentRouter.post("/posts/:postId/comments",isPostExisted, async (req,res,next)=>{
-    try{
-        const newComment = new commentSchema({
-            post: req.params.postId,
-            ...req.body,
-          })
-          const {_id}=await newComment.save()
-
-          const updatedPost= await postModel.findByIdAndUpdate(
-            req.params.postId,
-            {$push:{comments:_id}},
-            {new:true,runValidators:true}
+commentRouter.get(
+  "/posts/:postId/comments/:commentId",
+  isPostExisted,
+  async (req, res, next) => {
+    try {
+      const Comment = await commentSchema.findById(req.params.commentId);
+      if (Comment) {
+        res.send(Comment);
+      } else {
+        next(
+          createHttpError(
+            404,
+            `Comment with the id of ${req.params.commentId} not found`
           )
-          res.status(201).send(updatedPost)
-    }catch(err){
-        next(err)
-    }
-})
-
-
-commentRouter.get("/posts/:postId/comments",isPostExisted, async (req,res,next)=>{
-    try{
-   const Comments=await commentSchema.find()
-   res.send(Comments)
-    }catch(err){
-        next(err)
-    }
-})
-
-
-commentRouter.get("/posts/:postId/comments/:commentId",isPostExisted, async (req,res,next)=>{
-    try{
-     const Comment=await commentSchema.findById(req.params.commentId)
-     if(Comment){
-     res.send(Comment)
-     }else{
-        next(createHttpError(404,`Comment with the id of ${req.params.commentId} not found`))
-     }
-    }catch(err){
-        next(err)
-    }
-})
-
-
-commentRouter.put("/posts/:postId/comments/:commentId",isPostExisted, async (req,res,next)=>{
-    try{
-        const UpdatedComment=await commentSchema.findByIdAndUpdate(
-            req.params.commentId,
-            req.body,
-            { new: true, runValidators: true })
-        if(UpdatedComment){
-           res.send(UpdatedComment)
-        }else{
-           next(createHttpError(404,`Comment with the id of ${req.params.commentId} not found`))
-        }
-    }catch(err){
-        next(err)
-    }
-})
-
-
-commentRouter.delete("/posts/:postId/comments/:commentId",isPostExisted, async (req,res,next)=>{
-    try{
-      const Deleted=await commentSchema.findByIdAndDelete(req.params.commentId)
-
-      if(Deleted){
-        const updatedPost=await postModel.findByIdAndUpdate(
-            req.params.commentId,
-            {$pull:{comments:req.params.commentId}},
-            { new: true, runValidators: true })
-            res.status(204).send()
-      }else{
-        next(createHttpError(404,`Comment with the id of ${req.params.commentId} not found`))
-
+        );
       }
-    }catch(err){
-        next(err)
+    } catch (err) {
+      next(err);
     }
-})
+  }
+);
 
+commentRouter.put(
+  "/posts/:postId/comments/:commentId",
+  isPostExisted,
+  async (req, res, next) => {
+    try {
+      const UpdatedComment = await commentSchema.findByIdAndUpdate(
+        req.params.commentId,
+        req.body,
+        { new: true, runValidators: true }
+      );
+      if (UpdatedComment) {
+        res.send(UpdatedComment);
+      } else {
+        next(
+          createHttpError(
+            404,
+            `Comment with the id of ${req.params.commentId} not found`
+          )
+        );
+      }
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
-export default commentRouter
+commentRouter.delete(
+  "/posts/:postId/comments/:commentId",
+  isPostExisted,
+  async (req, res, next) => {
+    try {
+      const Deleted = await commentSchema.findByIdAndDelete(
+        req.params.commentId
+      );
+
+      if (Deleted) {
+        const updatedPost = await postModel.findByIdAndUpdate(
+          req.params.commentId,
+          { $pull: { comments: req.params.commentId } },
+          { new: true, runValidators: true }
+        );
+        res.status(204).send();
+      } else {
+        next(
+          createHttpError(
+            404,
+            `Comment with the id of ${req.params.commentId} not found`
+          )
+        );
+      }
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+export default commentRouter;
--- src/api/Comments/model.js
+++ src/api/Comments/model.js
@@ -1,13 +1,22 @@
-import mongoose, { model, Types } from "mongoose"
+import mongoose, { model, Types } from "mongoose";
 
-const {Schema}=mongoose
+const { Schema } = mongoose;
 
+const commentSchema = new Schema(
+  {
+    user: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+    comment: { type: String, required: true, minLength: 5, maxLength: 20 },
+    post: { type: mongoose.Types.ObjectId, required: true, ref: "Post" },
+  },
+  { timeStamps: true }
+);
 
-const commentSchema=new Schema({
-    user:{type:mongoose.Types.ObjectId,required:true, ref:"User"},
-    comment:{type:String, required:true,minLength: 5, maxLength:20},
-    post:{type:mongoose.Types.ObjectId,required:true, ref:"Post"},
-},{timeStamps:true})
+commentSchema.static("getCommentsWithUserDetails", async function () {
+  const comments = await this.find().populate({
+    path: "user",
+    select: "name surname image",
+  });
+  return comments;
+});
 
-
-export default model("Comment",commentSchema)
+export default model("Comment", commentSchema);
