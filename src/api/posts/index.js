eea6995590cf75608844c7c5b095cb007224e3b4
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2023-03-22 16:05:12 +0100

    populated comments in all post

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -37,8 +37,15 @@ PostsRouter.get("/posts", async (req, res, next) => {
       .populate({ path: "comments", select: "comment user" })
       .populate({ path: "user", select: " name surname  image  _id title" })
       .populate({
-        path: "comments.user likes",
+        path: "comments user likes",
         select: "name surname image",
+      })
+      .populate({
+        path: "comments",
+        populate: {
+          path: "user",
+          select: "name surname image",
+        },
       });
 
     const total = await PostModel.countDocuments(mongoQuery.criteria);
